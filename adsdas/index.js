855b566b3ce4cca43bbbe973daa301eeba083c4e
Animeshmitra08 <84204071+Animeshmitra08@users.noreply.github.com>
2023-08-20 16:24:41 +0530

    Add files via upload

---
+++ adsdas/index.js
@@ -0,0 +1,16 @@
+// const sub_title = document.querySelector(".sub-title");
+
+// const typewriter = () =>{
+//     setTimeout(() => {
+//         sub_title.textContent = "Front-end Web Developer"
+//     }, 0);
+// }
+
+// typewriter();
+
+var typed = new Typed('.sub-title', {
+    strings: ['Front-end Web Developer', 'Back-end Developer', 'Designer', 'Data Scientist'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+  });
